332cc1ab4a47c83e1f020838016131446f9d0706
aashish-tiwary <110485513+aashish-tiwary@users.noreply.github.com>
2022-12-05 20:15:02 +0530

    Add files via upload

--- verilog/rtl/user_proj_example.v
+++ verilog/rtl/user_proj_example.v
@@ -68,98 +68,54 @@ module user_proj_example #(
     // IRQ
     output [2:0] irq
 );
-    wire clk;
-    wire rst;
 
-    wire [`MPRJ_IO_PADS-1:0] io_in;
-    wire [`MPRJ_IO_PADS-1:0] io_out;
-    wire [`MPRJ_IO_PADS-1:0] io_oeb;
+wire clk;
+wire [7:0] A,B;
+wire [2:0] op;
+wire [7:0] R;
+assign {clk,A,B,op} = io_in[`MPRJ_IO_PADS-1:18];
+assign io_out[`MPRJ_IO_PADS-1:30] = R;
 
-    wire [31:0] rdata; 
-    wire [31:0] wdata;
-    wire [BITS-1:0] count;
-
-    wire valid;
-    wire [3:0] wstrb;
-    wire [31:0] la_write;
-
-    // WB MI A
-    assign valid = wbs_cyc_i && wbs_stb_i; 
-    assign wstrb = wbs_sel_i & {4{wbs_we_i}};
-    assign wbs_dat_o = rdata;
-    assign wdata = wbs_dat_i;
-
-    // IO
-    assign io_out = count;
-    assign io_oeb = {(`MPRJ_IO_PADS-1){rst}};
-
-    // IRQ
-    assign irq = 3'b000;	// Unused
-
-    // LA
-    assign la_data_out = {{(127-BITS){1'b0}}, count};
-    // Assuming LA probes [63:32] are for controlling the count register  
-    assign la_write = ~la_oenb[63:32] & ~{BITS{valid}};
-    // Assuming LA probes [65:64] are for controlling the count clk & reset  
-    assign clk = (~la_oenb[64]) ? la_data_in[64]: wb_clk_i;
-    assign rst = (~la_oenb[65]) ? la_data_in[65]: wb_rst_i;
-
-    counter #(
-        .BITS(BITS)
-    ) counter(
-        .clk(clk),
-        .reset(rst),
-        .ready(wbs_ack_o),
-        .valid(valid),
-        .rdata(rdata),
-        .wdata(wbs_dat_i),
-        .wstrb(wstrb),
-        .la_write(la_write),
-        .la_input(la_data_in[63:32]),
-        .count(count)
-    );
+iiitb_alu instance(clk,A,B,op,R);
 
 endmodule
 
-module counter #(
-    parameter BITS = 32
-)(
-    input clk,
-    input reset,
-    input valid,
-    input [3:0] wstrb,
-    input [BITS-1:0] wdata,
-    input [BITS-1:0] la_write,
-    input [BITS-1:0] la_input,
-    output ready,
-    output [BITS-1:0] rdata,
-    output [BITS-1:0] count
-);
-    reg ready;
-    reg [BITS-1:0] count;
-    reg [BITS-1:0] rdata;
+module iiitb_alu(
+clk,
+A,
+B,
+op,
+R   );
+    
+//inputs,outputs and internal variables declared here
+input clk;
+input [7:0] A,B;
+input [2:0] op;
+output [7:0] R;
+wire [7:0] Reg1,Reg2;
+reg [7:0] Reg3;
+    
+//Assign A and B to internal variables for doing operations
+assign Reg1 = A;
+assign Reg2 = B;
+//Assign the output 
+assign R = Reg3;
 
-    always @(posedge clk) begin
-        if (reset) begin
-            count <= 0;
-            ready <= 0;
-        end else begin
-            ready <= 1'b0;
-            if (~|la_write) begin
-                count <= count + 1;
-            end
-            if (valid && !ready) begin
-                ready <= 1'b1;
-                rdata <= count;
-                if (wstrb[0]) count[7:0]   <= wdata[7:0];
-                if (wstrb[1]) count[15:8]  <= wdata[15:8];
-                if (wstrb[2]) count[23:16] <= wdata[23:16];
-                if (wstrb[3]) count[31:24] <= wdata[31:24];
-            end else if (|la_write) begin
-                count <= la_write & la_input;
-            end
-        end
-    end
-
+//Always block with inputs in the sensitivity list.
+always @(posedge clk)
+begin
+case (op)
+0 : Reg3 = Reg1 + Reg2;  //addition
+1 : Reg3 = Reg1 - Reg2; //subtraction
+2 : Reg3 = ~Reg1;  //NOT gate
+3 : Reg3 = ~(Reg1 & Reg2); //NAND gate 
+4 : Reg3 = ~(Reg1 | Reg2); //NOR gate               
+5 : Reg3 = Reg1 & Reg2;  //AND gate
+6 : Reg3 = Reg1 | Reg2;  //OR gate    
+7 : Reg3 = Reg1 ^ Reg2; //XOR gate  
+endcase 
+end
+    
 endmodule
+    
 `default_nettype wire
